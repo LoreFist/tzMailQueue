3d447226532c2c302d3cd8ed5f1501b9adefe68c
Timofey Krischenko <timzipa@gmail.com>
2013-02-28 11:38:30 +0700

    fixes2

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ $yii=dirname(__FILE__).'/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
 
 defined('YII_DEBUG') or define('YII_DEBUG',true);
-defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',0);
 
 require_once($yii);
 Yii::createWebApplication($config)->run();
